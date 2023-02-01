71471b48187eda947c3c47d44ed0ca3e0ba83066
Luis Ligunas <luisrainier.ligunas@gmail.com>
2023-02-01 09:23:31 +0800

    Initial Commit

---
+++ Layout Test App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Layout Test App
+//
+//  Created by Luis Ligunas on 2/1/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
